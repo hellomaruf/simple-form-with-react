287cf2660ad8fd076f0103891a6bd62acb935ea9
maruf-ahmed <marufahamed1110@gmail.com>
2024-03-18 13:43:03 +0600

    handle signin button

--- src/components/registration/Registratioin.jsx
+++ src/components/registration/Registratioin.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function Registratioin() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    console.log(name, email, password);
+    event.preventDefault();
+  };
   return (
     <div>
       <h2 className="text-3xl font-bold py-6">Create an Account</h2>
-      <form action="" className="w-96">
+      <form
+        onSubmit={(event) => handleSubmit(event)}
+        action=""
+        className="w-96"
+      >
         <label className="" htmlFor="">
           Name*
         </label>
@@ -10,18 +23,21 @@ function Registratioin() {
           className="bg-gray-200 pl-4 rounded-xl h-12 w-full mb-6 outline-none focus:bg-slate-100"
           placeholder="Enter Your Name"
           type="text"
+          onChange={(event) => setName(event.target.value)}
         />
         <label htmlFor="">Email*</label>
         <input
           className="bg-gray-200 pl-4 rounded-xl h-12 w-full mb-6 outline-none focus:bg-slate-100"
           placeholder="Enter Your Email"
           type="email"
+          onChange={(event) => setEmail(event.target.value)}
         />
         <label htmlFor="">Password*</label>
         <input
           className="bg-gray-200 pl-4 rounded-xl h-12 w-full mb-6 outline-none focus:bg-slate-100"
           placeholder="Enter Your password"
           type="password"
+          onChange={(event) => setPassword(event.target.value)}
         />
         <button className="bg-[#693DF5] text-white py-2 px-8 rounded-full font-bold">
           sign in
